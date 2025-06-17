06f481a1f3acff4233aebebbd656697aa5b234b4
Duvier David <duvierdiegodavid.0715@gmail.com>
2025-06-16 21:47:33 -0500

    INICIANDO WINDOWS

---
+++ src/vistas/VentanaPrincipal.java
@@ -0,0 +1,10 @@
+package vistas;
+
+import javax.swing.*;
+
+public class VentanaPrincipal {
+    private JPanel panelVentana;
+    private JLabel lblTitulo;
+    private JButton btnGestionarPersonas;
+    private JButton btnGestionarMascotas;
+}
